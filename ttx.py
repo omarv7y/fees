4d0cee02c0ecac58396bd74d62a5b96abc619088
omarv7y <70105391+omarv7y@users.noreply.github.com>
2021-02-02 12:43:58 +0200

    Update ttx.py

--- ttx.py
+++ ttx.py
@@ -10,8 +10,8 @@ print ('omar'*100)
 os.system("cd checkversion && wget https://github.com/omarv7y/fees/blob/main/omar.txt")
 s=open("checkversion/omar.txt","r")
 version=s.read()
-current-version=open("omar.txt","r").read()
-if version!=current-version:
+current_version=open("omar.txt","r").read()
+if version!=current_version:
       os.system("rm ttx.py omar.txt && git clone https://github.com/omarv7y/fees.git")
       print("updated")
 else:
